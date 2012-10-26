5e110579bf74e3eeb16b8cb4383d099d8da60077
Daniel <danieldrew410@gmail.com>
2012-10-26 18:08:40 -0300

    Update js/app.js
    I have added in 2 regular ajax requests to our dev server so that I 
    could start testing and setting up the patient selection. I am not sure
     what the best way to assign values from an ajax response to the 
    corresponding fields,I assume that using some type of template system would work well so that we could just assign the values to variables instead of having to look up the field each time.
    
    I did try to get this to work in the playbook browser but was having 
    some issues, I will look into what could be causing these next week.

--- js/app.js
+++ js/app.js
@@ -25,6 +25,9 @@ $(document).ready(function() {
 	$("#pause").bind("click", app.pause);
 	$("#stop").bind("click", app.stop);
 	$("#play").bind("click", app.play);
+	$("#findPatient").bind("click", app.findPatient);
+	$("#acceptPatient").bind("click", app.findAdmit);
+
 });
 
 var app = {
@@ -33,6 +36,16 @@ var app = {
 	deviceType : 'Browser',
 	filePath : '/',
 	fileName : 'chief_complaint.wav',
+	patient : new function(){
+		this.patientID = "None";
+		this.admitID = "";
+		this.patientIndex = "";
+		this.newAdmit = false;
+		this.newPatient = false;
+		this.lastName = "";
+	},
+
+
 
 	init : function() {
 		//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -56,6 +69,8 @@ var app = {
 		}
 		// Send to desired home page once page is loaded
 		window.setTimeout(app.homePage, 200);
+
+
 	},
 
 	homePage : function() {
@@ -147,5 +162,151 @@ var app = {
 			return;
 		}
 		console.log('error code:' + errorCode + ' error message:' + errorMessage);
+	},
+
+
+
+	findPatient: function(){
+		var patientID = $('#patient_id').val();
+		app.patient.lastName = $('#Field1').val();
+		var params = new function(){ this.patientID = ""; this.lastName = "";};
+		app.patient.dob = $('#Field2').val();
+		params.patientID = patientID;
+		params.lastName = app.patient.lastName;
+
+		if (app.patient.patientIndex != "" || app.patient.lastName != ""){
+			$.ajax({
+			     url: 'findPatient',
+			     type: "POST",
+			     dataType: "json",
+			     crossDomain: false,
+			     data: params,
+			     cache: false,
+			     success: function(response) {
+					var patientSearch = "<input id='patSelect' name='patientSel' type='hidden' value='' />";
+			     	if (response.newPatient === false){
+				        var mr = response.results.mR,
+				        first = response.results.firstNames,
+				        last = response.results.lastNames,
+				        dob = response.results.dob,
+				        ssn = response.results.ssn,
+				        patId = response.results.patID, 
+				        options = { 
+				        	beforeclose: function(){
+				        		console.log('before close');
+				        		app.patient.patientIndex = $('#patSearchList input:checked ').attr("data-patientID");
+				        		if (app.patient.patientIndex == "newPatient"){
+				        			app.patient.newPatient = true;
+				        			app.patient.newAdmit = true;
+				        		}
+
+				        	}};
+				        
+				        for (var x=0; x<response.results.mR.length; x++){
+				        	patientSearch+= "<span><input type='radio' class='field radio patientChoice' name='patientSel' data-patientID = "+patId[x]+" id='patient" +patId[x]+"'/> \
+							   <label for='patient" +patId[x]+"' class='choice' >Patient ID:"+mr[x] + " Name:" +last[x]+", "+first[x] + " Dob:"+dob[x] + " Ssn:" + ssn[x] +"</label></span>";
+				        }			     		
+			     	}
+			        
+			        patientSearch+= "<span> <input type='radio' class='field radio patientChoice' name = 'patientSel' data-patientID = 'newPatient' for = 'patientSel' id = 'newPatient'/>\
+								   	<label for='newPatient'>New Patient</label></span>";
+					
+					$("#patSearchList").empty().append(patientSearch);
+					$('.patientChoice').checkboxradio();	
+		  		    $("#patientsearch").dialog(options);
+			    	$.mobile.changePage("#patientsearch");
+			        console.log("Patient Search Successful" + response);
+			     }
+			     // },
+			     // error: function(e) {
+			     //    console.log('Error on find Patient satus:'+e.status);
+			     //    console.log(e);
+			     // }
+			 });
+
+		}
+	},
+	findAdmit : function(){
+		app.patient.patientIndex = $('#patSearchList input:checked ').attr("data-patientID");
+   		if (app.patient.patientIndex == "newPatient"){
+   			app.patient.newPatient = true;
+   			app.patient.newAdmit = true;
+   		}
+
+		params = new function(){
+				this.patientID = app.patient.patientIndex,
+				this.lastName = app.patient.lastName,
+				this.dob = app.patient.dob,
+				this.newPatient =app.patient.newPatient,
+				this.newAdmit = app.patient.newAdmit;
+		};
+
+
+					$.ajax({
+				    url: 'findAdmit',
+				    type: "POST",
+				    dataType: "json",
+				    crossDomain: false,
+				    data: params,
+				    cache: false,
+				    success: function(response){
+				    	if (response.results){
+
+
+					    	if (response.newAdmit === false){
+					    	
+					    		var admitHeader = "<h1>Please select Admission</h1>";
+					    	
+					    	}else{
+					    	
+					    		var admitHeader = "<h1>Please select an ADT Message</h1>";
+					    	
+					    	}
+
+					    	$('#admitSelectHeader').html(admitHeader);
+
+
+					    		var admitDate = response.results.admitDates,
+					    		billingNumber = response.results.billingNumbers,
+					    		admitIndex = response.results.admitIndex,
+					    		status = response.results.inPatient,
+					    		admitSearch = "<input id='patSelect' name='admitSel' type='hidden' value='' />";
+					    		if (response.results.hasOwnProperty('firstName')){
+					    			var firstName = response.results.firstName;
+							        for (var x=0; x<response.results.firstName.length; x++){
+
+							        	admitSearch+= "<span><input type='radio' class='field radio admitChoice' name='admitSel' data-admitID = "+admitIndex[x]+" id='admit"+x+"'/> \
+										   <label for='admit" +x+"' class='choice' >First Name: "+firstName[x] + " Admit Date:" +admitDate[x]+" Type: "+status[x] + " Billing Number:"+billingNumber[x] +"</label></span>";
+							        }				    			
+
+					    		}else{
+							        
+							        for (var x=0; x<status.length; x++){
+
+							        	admitSearch+= "<span><input type='radio' class='field radio admitChoice' name='admitSel' data-admitID = "+admitIndex[x]+" id='admit"+x+"'/> \
+										   <label for='admit" +x+"' class='choice' >Admit Date:" +admitDate[x]+" Type: "+status[x] + " Billing Number:"+billingNumber[x] +"</label></span>";
+							        }
+
+					    		}
+	     		
+					        
+							    admitSearch+= "<span> <input type='radio' class='field radio admitChoice' name = 'admitSel' data-admitID = 'newAdmit' for = 'patientSel' id = 'newAdmit'/>\
+									<label for='newAdmit'>New Admission</label></span>";
+
+
+							   	$("#admitSearchList").empty().append(admitSearch);
+							   	$('.admitChoice').checkboxradio();
+								$("#admitSelect").dialog();
+								$.mobile.changePage("#admitSelect");
+						}
+
+					},
+				    error: function(e){
+				    	console.log('error on find admit'+ e.status);
+				    	console.log(e);
+				    }
+
+				});
+
 	}
 }
